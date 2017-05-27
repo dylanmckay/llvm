f614b2f7c68fa413484ace6fa070f05e825ccc1e
Dr. ERDI Gergo <gergo@erdi.hu>
2017-05-27 21:02:50 +0800

    [AVR] Use 1-byte alignment for all data types (https://github.com/avr-rust/rust/issues/64)

--- lib/Target/AVR/AVRTargetMachine.cpp
+++ lib/Target/AVR/AVRTargetMachine.cpp
@@ -25,7 +25,7 @@
 
 namespace llvm {
 
-static const char *AVRDataLayout = "e-p:16:16-i8:8-i16:16-i32:16-i64:16-f32:16-f64:16-n8-a:16";
+static const char *AVRDataLayout = "e-p:16:8-i8:8-i16:8-i32:8-i64:8-f32:8-f64:8-n8-a:8";
 
 /// Processes a CPU name.
 static StringRef getCPU(StringRef CPU) {
